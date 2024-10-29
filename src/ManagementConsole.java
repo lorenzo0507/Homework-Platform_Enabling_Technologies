01336814fbce851f1733e19e2b6c18b4b03d1927
lorenzo0507 <lorenzo0507@gmail.com>
2024-10-29 11:49:06 +0100

    low battery mini commit

--- src/ManagementConsole.java
+++ src/ManagementConsole.java
@@ -1,9 +1,40 @@
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Objects;
 
 public class ManagementConsole extends Thread {
+
+    void loadServlet(String name) {
+        // Check table for servlet
+
+        // If found, print error message, else attempt loading
+        if(tpbsc.servletHashtable.containsKey(name)) {
+            System.out.println(name + " is already loaded.");
+            return;
+        }
+
+        File dir = new File(System.getProperty("user.dir") + File.separator + "servletcontainer" + File.separator);
+
+        File[] arr = dir.listFiles();
+
+        // find servlet in repository, check metadata.txt for class name and load it.
+
+    }
+
+    void unloadServlet(String name) {
+
+        if(!tpbsc.servletHashtable.containsKey(name)) {
+            System.out.println(name + " is not loaded.");
+            return;
+        }
+
+        // TODO Actually unload the servlet class
+
+        tpbsc.servletHashtable.remove(name);
+    }
+
 
     public void execute(String cmd) {
 
